9f03ebfdf7e3ece2604f5dd3c260c353d4c24318
Stepan <stepan.gavriusenco@gmail.com>
2023-11-21 18:29:58 +0200

    lab2(frame)

--- migrations/Version20231011182404.php
+++ migrations/Version20231011182404.php
@@ -8,7 +8,6 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
  */
 final class Version20231011182404 extends AbstractMigration
 {
